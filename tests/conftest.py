9dee98b1c69ce8cb753f2f421410cd364bd823fa
Curasao <rudarya4@gmail.com>
2026-03-02 02:05:28 +0300

    Fix test_reg.py and conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,19 @@
 import pytest
-from selene import browser
+from selene import browser, have, be
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture(scope='function')
 def setup_browser_landing():
-    browser.config.base_url = "https://www.povarenok.ru"
-    browser.config.window_width = 1920
-    browser.config.window_height = 1080
-    browser.config.timeout = 10
+      options = Options()
+      options.headless = True
+      driver = webdriver.Chrome(options=options)
+      driver.set_window_size(1920, 1080)
+      browser.config.driver = driver
+      browser.config.base_url = "https://www.povarenok.ru"
+      browser.config.timeout = 30
 
-    yield browser
+      yield browser
 
-    browser.quit()
+      browser.quit()
--- tests/test_registration_page.py
+++ tests/test_registration_page.py
@@ -1,26 +1,32 @@
 from asyncio import timeout
-from selene import browser, have, be
+from selene import browser, have, be,command
 import pytest
 import allure
 
 
 class AuthPage:
-    def __init__(self):
-        self.login_field = browser.element('#tl_login_id')
-        self.password_field = browser.element('#tl_pwd_id')
-        self.submit_button = browser.element('[value="Войти на сайт"]')
-
-    def open(self, setup_browser_landing):
+    def open(self):
         browser.open('https://www.povarenok.ru')
 
-    def fill_login(self, email_or_username, setup_browser_landing):
-        browser.element(self.login_field).type('bellkapd-2026')
+    def fill_login(self):
+        browser.element('#tl_login_id').should(be.visible).type('bellkapd-2026')
 
-    def fill_password(self, password, setup_browser_landing):
-        browser.element(self.password_field).type('testoviy-2026')
 
-    def submit_auth(self):
-        browser.element(self.submit_button).click()
+    def fill_password(self):
+        browser.element('#tl_pwd_id').should(be.visible).type('testoviy2026')
+
+
+    def press_button(self):
+        browser.element('input[value="Войти на сайт"]').should(be.visible).click()
+
+    def add_recipe(self):
+        browser.element('.sbm-link').should(be.visible).click()
+
+    def fill_rek_login(self):
+        browser.element('.table>tbody>tr>td>input[placeholder="Ваше имя"]').should(be.visible).with_(timeout=30).type('1111')
+
+    def fill_rek_email(self):
+        browser.element('.table>tbody>tr>td>input[placeholder="Ваш e-mail"]').should(be.visible).with_(timeout=30).type('test@yandex.ru')
 
     def check_success(self):
         browser.element('b').should(have.text('Здравствуйте, bellkapd-2026'))
@@ -30,22 +36,22 @@ class AuthPage:
 
 
 @allure.title('Авторизация с корректными данными')
-def test_valid_login():
+def test_valid_login(setup_browser_landing):
     auth_page = AuthPage()
     auth_page.open()
-    browser.element('.dr__cross').with_(timeout=30).should(be.visible).click()
-    auth_page.fill_login('bellkapd-2026@yandex.ru')
-    auth_page.fill_password('testoviy2026')
-    auth_page.submit_auth()
+    auth_page.fill_login()
+    auth_page.fill_password()
+    auth_page.press_button()
     auth_page.check_success()
 
 
 @allure.title('Авторизация с некорректными данными')
-def test_invalid_login():
+def test_invalid_login(setup_browser_landing):
     auth_page = AuthPage()
     auth_page.open()
-    browser.element('.dr__cross').with_(timeout=30).should(be.visible).click()
-    auth_page.fill_login('bad_email@example.com')
-    auth_page.fill_password('incorrect_pass')
-    auth_page.submit_auth()
+    auth_page.fill_login()
+    auth_page.fill_password()
+    auth_page.press_button()
     auth_page.check_failure()
+
+
